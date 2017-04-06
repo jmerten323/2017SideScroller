adab43d3775445342467cb81069cec06c2c3cb25
Unknown <mertenj1@hawkmail.newpaltz.edu>
2017-04-06 12:06:07 -0400

    Made coins have different values

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour {
-
+    public int pointsValue; 
 
     void OnCollisionEnter2D(Collision2D coll)
     {
@@ -11,7 +11,7 @@ public class Coin : MonoBehaviour {
         if (player != null)
         {
             gameObject.SetActive(false);
-            FindObjectOfType<GM>().SetPoints(FindObjectOfType<GM>().GetPoints() + 1);
+            FindObjectOfType<GM>().SetPoints(FindObjectOfType<GM>().GetPoints() + pointsValue);
         }
 
 
