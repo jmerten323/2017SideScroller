474412683dd8f2094301a968edf121ddb5c34180
Unknown <mertenj1@hawkmail.newpaltz.edu>
2017-04-13 10:47:49 -0400

    Flying code

---
+++ Assets/Scripts/FlyingBooster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBooster : MonoBehaviour {
+
+    float timeStarted = 0;
+    Player player;
+    public float lastforSeconds = 10;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        player = coll.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.canFly = true;
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            timeStarted = Time.time; 
+        }
+
+
+    }
+    void Update()
+    {
+        if(timeStarted != 0 && timeStarted + lastforSeconds < Time.time)
+        {
+            timeStarted = 0;
+            player.canFly = false;
+
+        }
+    }
+}
