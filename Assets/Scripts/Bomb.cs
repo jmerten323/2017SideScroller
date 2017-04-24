e895f78f2eea1221ae391085369e809d19098dd9
Unknown <mertenj1@hawkmail.newpaltz.edu>
2017-04-24 10:41:58 -0400

    Throw and Explode Bomb

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,20 +4,71 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour {
 
+    public float blastRadius = 4;
+    public bool isActive = false;
+
+    private new Rigidbody2D rigidbody2D;
+    private new Collider2D collider;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && isActive)
+        {
+            Throw();
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         var player = coll.gameObject.GetComponent<Player>();
-        if (player != null)
+        if (player != null && !isActive)
         {
-            var enemies = FindObjectsOfType<Enemy>();
-            foreach(var e in enemies)
-            {
-                 if(Vector3.Distance(this.transform.position, e.transform.position) < 4) {
-                    e.gameObject.SetActive(false);
-                }
-            }
+            Getpickedup(player);
+        }
+        if (isActive && player == null)
+        {
+            Explode();
         }
 
+        }
 
+    public void Throw()
+    {
+        transform.parent = null;
+        rigidbody2D.velocity = new Vector2(5, 0);
+        rigidbody2D.isKinematic = false;
+        collider.enabled = true;
+    }
+
+
+    public void Getpickedup(Player player)
+    {
+        Debug.Log("Getpickedup");
+        isActive = true;
+        transform.parent = player.transform;
+        transform.localScale = new Vector3(.1f, .1f);
+        transform.localPosition = new Vector3(.2f, .2f);
+        rigidbody2D.velocity = new Vector2();
+        rigidbody2D.isKinematic = true;
+        collider.enabled = false;
+    }
+    
+        public void Explode()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+        foreach (var e in enemies)
+        {
+            if (Vector3.Distance(this.transform.position, e.transform.position) < blastRadius)
+            {
+                e.gameObject.SetActive(false);
+            }
+        }
+        gameObject.SetActive(false);
     }
 }
